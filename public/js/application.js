1ab3d121850061ab26419b099f4d56b61dbdb9f2
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-04 01:00:25 -0700

    ADD script to request for data on set interval and insert data into DOM

--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,42 @@
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+
+var inputTextboxListener = function() {
+  $(".dice-token-count").on("change", function() {
+    var userInput = $(this).val();
+    $(".dice-token-count").val(userInput);
+  });
+};
+
+var dataChangeListener = function() {
+  $(".bold").on("DOMNodeInserted", function() {
+    $(".bold").css("background-color", "lightgrey");
+  });
+};
+
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
-
-
-  /*eventListener();*/
+  inputTextboxListener();
+  dataChangeListener();
 });
 
+var requestData = function(){
+  var sendGetRequest = $.get('/requestinfo');
 
-// function eventListener() {
-//   $('form').on('click', 'button', function(e) {
-//     e.prevenDefault();
+  sendGetRequest.done(function(response) {
+    $('#quarterly-profits-usd').html('$' + (response['eth_rate']*response['quarterly_profits_eth']).formatMoney(2));
+    $('#quarterly-profits-eth').html(response['quarterly_profits_eth'].toFixed(6) + 'ETH');
+    $('#current-eth-rate').html('$' + response['eth_rate'].formatMoney(2));
+    $('#current-dice-rate').html(response['dice_rate_eth'].toFixed(6) + 'ETH');
+  });
+}; 
 
-//     var form = $(this).closest('form')
-
-//     var request = $.ajax({
-//       method: form.attr('method'),
-//       url: form.attr('action'),
-//       data: form.serialize()
-//     });
-
-//     request.done(function(response) {
-
-//     });
-
-//     request.done(function() {
-//       alert('Failed to perform action');
-//     })
-//   });
-// };
+window.setInterval(function(){requestData()}, 1000);
