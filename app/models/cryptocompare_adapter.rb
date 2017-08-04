50917a11c8886d2452bea4d737a19d78f84b48cc
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-04 15:31:03 -0700

    ADD method to cryptocomare adapter model

--- app/models/cryptocompare_adapter.rb
+++ app/models/cryptocompare_adapter.rb
@@ -11,4 +11,17 @@ class CryptocompareAdapter
     response = JSON.parse(http.request(request).body)
     return response
   end
+
+  def eth_in_idr
+    url = URI("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=IDR")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+
+    response = JSON.parse(http.request(request).body)
+    return response
+  end
 end
