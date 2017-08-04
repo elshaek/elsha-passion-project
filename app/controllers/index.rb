1cdd6f85d17eca9f34e36587bbf7bb56fe82fe8d
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-04 02:46:06 -0700

    UPDATE response json

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,11 +3,12 @@ get '/' do
 end
 
 get '/requestinfo' do
+  @total_dice_supply = EtherscanAdapter.new.get_total_dice_supply
   @quarterly_profits_eth = EtherscanAdapter.new.get_bankroll_balance - 1200
   @eth_rate = EtherscanAdapter.new.get_ether_price
   @dice_rate_eth = CryptocompareAdapter.new.dice_value['ETH']
   @dice_rate_usd = CryptocompareAdapter.new.dice_value['USD']
 
   content_type :json
-  { quarterly_profits_eth: @quarterly_profits_eth, eth_rate: @eth_rate, dice_rate_eth: @dice_rate_eth }.to_json
+  { total_dice_supply: @total_dice_supply, quarterly_profits_eth: @quarterly_profits_eth, eth_rate: @eth_rate, dice_rate_eth: @dice_rate_eth, dice_rate_usd: @dice_rate_usd }.to_json
 end
