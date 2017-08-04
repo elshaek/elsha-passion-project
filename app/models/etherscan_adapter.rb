56372eb666869cfc904cec779f1e3c6fc24f87c6
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-04 01:02:00 -0700

    Comment out original code for get_ether_price method and use alternative code

--- app/models/etherscan_adapter.rb
+++ app/models/etherscan_adapter.rb
@@ -20,8 +20,12 @@ class EtherscanAdapter
   end
 
   def get_ether_price
-    @ether_price_options = { query: { 'module': 'stats', 'action': 'ethprice', 'apikey': @api_key }}
-    response = self.class.get("/api", @ether_price_options)
+    # @ether_price_options = { query: { 'module': 'stats', 'action': 'ethprice', 'apikey': @api_key }}
+    # response = self.class.get("/api", @ether_price_options)
+    # return response["result"]["ethusd"].to_f
+
+    response = HTTParty.get("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=#{@api_key}")
+
     return response["result"]["ethusd"].to_f
   end
 end
