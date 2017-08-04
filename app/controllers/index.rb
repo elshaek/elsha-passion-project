8b2948200467e8adec7eea592d3c52267d436657
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-04 15:31:24 -0700

    ADD get '/idr' route handler

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,8 @@
 get '/' do
+  redirect '/usd'
+end
+
+get '/usd' do
   erb :index
 end
 
@@ -8,7 +12,13 @@ get '/requestinfo' do
   @eth_rate = EtherscanAdapter.new.get_ether_price
   @dice_rate_eth = CryptocompareAdapter.new.dice_value['ETH']
   @dice_rate_usd = CryptocompareAdapter.new.dice_value['USD']
+  @dice_rate_idr = CryptocompareAdapter.new.dice_value['IDR']
+  @eth_rate_idr = CryptocompareAdapter.new.eth_in_idr['IDR']
 
   content_type :json
-  { total_dice_supply: @total_dice_supply, quarterly_profits_eth: @quarterly_profits_eth, eth_rate: @eth_rate, dice_rate_eth: @dice_rate_eth, dice_rate_usd: @dice_rate_usd }.to_json
+  { total_dice_supply: @total_dice_supply, quarterly_profits_eth: @quarterly_profits_eth, eth_rate: @eth_rate, dice_rate_eth: @dice_rate_eth, dice_rate_usd: @dice_rate_usd, dice_rate_idr: @dice_rate_idr, eth_rate_idr: @eth_rate_idr }.to_json
+end
+
+get '/idr' do
+  erb :idr
 end
