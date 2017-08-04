3c7415a6f2d1292831d3c538e4572d9ac953e350
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-04 01:02:37 -0700

    UPDATE get '/requestinfo' route handler

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,13 @@
 get '/' do
-  erb :'/index'
+  erb :index
+end
+
+get '/requestinfo' do
+  @quarterly_profits_eth = EtherscanAdapter.new.get_bankroll_balance - 1200
+  @eth_rate = EtherscanAdapter.new.get_ether_price
+  @dice_rate_eth = CryptocompareAdapter.new.dice_value['ETH']
+  @dice_rate_usd = CryptocompareAdapter.new.dice_value['USD']
+
+  content_type :json
+  { quarterly_profits_eth: @quarterly_profits_eth, eth_rate: @eth_rate, dice_rate_eth: @dice_rate_eth }.to_json
 end
